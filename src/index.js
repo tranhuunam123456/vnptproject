c07aacd1506bbc84125bcc689fba1c6bbce2020e
tranhuunam123456 <72679827+tranhuunam123456@users.noreply.github.com>
2021-07-23 17:10:47 +0700

    2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express')
-
+const morgan = require('morgan')
+var exphbs = require('express-handlebars');
+const path = require("path")
 const app = express()
 const port = 3000
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(morgan('combined'))
+app.engine('hbs', exphbs({
+    extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", function(req, res) {
+    res.render('home')
 })
-
+app.get('/home', function(req, res) {
+    res.render('home')
+})
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
