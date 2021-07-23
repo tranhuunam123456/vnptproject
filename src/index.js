910804cc2d199dd72689ab4a645deeeffcadf8be
tranhuunam123456 <72679827+tranhuunam123456@users.noreply.github.com>
2021-07-23 09:19:14 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
-const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.get('/', function(req, res) {
-    res.send('helo')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
